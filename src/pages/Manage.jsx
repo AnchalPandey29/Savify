bb40121e0ab0b961ae215c1228b814a4e41fccd4
AnchalPandey29 <anchal29pandey@gmail.com>
2025-09-12 13:52:46 +0530

    next changes

--- src/pages/Manage.jsx
+++ src/pages/Manage.jsx
@@ -1,8 +1,34 @@
 import React from 'react'
 
 const Manage = () => {
+  //finace manage form and list another side
   return (
-    <div>Manage</div>
+    <div className='flex gap-3 bg-gray-200 p-6 rounded-lg'>
+
+      <div className='bg-white p-6 rounded-lg shadow-md flex-1  max-w-md'>
+          <form className='flex flex-col gap-4'>
+              <input type="text" placeholder="Enter title" className='p-2 border border-gray-300 rounded' />
+              <input type="number" placeholder="Enter amount" className='p-2 border border-gray-300 rounded' />
+              <select className='p-2 border border-gray-300 rounded'>
+                <option>Select category</option>
+                <option value="food">Food</option>
+                <option value="transport">Transport</option>
+                <option value="utilities">Utilities</option> 
+                <option value="transport">Transport</option>
+                <option value="utilities">Utilities</option>
+              </select>
+              <input type="date" className='p-2 border border-gray-300 rounded'  />
+
+              <textarea placeholder="Enter description" className='p-2 border border-gray-300 rounded' />
+              <button type="submit" className='bg-blue-500 text-white p-2 rounded'>Submit</button>
+          </form>
+      </div>
+
+      <div className='bg-white p-6 rounded-lg shadow-md flex-1'>
+
+      </div>
+
+    </div>
   )
 }
 
